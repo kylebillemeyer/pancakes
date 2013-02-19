809da7c7b02f8f6c36e9bff06cf71bd8eb16c94e
Kyle Billemeyer <billemeyer.k@gmail.com>
2013-02-19 01:08:59 -0500

    moved Core project under src dir

---
+++ src/Pancakes.Engine.Core/Caching/IResourceCache.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pancakes.Engine.Caching
+{
+    public interface IResourceCache<T>
+    {
+        T GetResource(string key);
+        IEnumerable<Tuple<string, T>> GetResources();
+        IEnumerable<T> GetResources(IEnumerable<string> keys);
+
+        void AddResource(string key, T resource);
+        void AddResources(IEnumerable<Tuple<string, T>> resources);
+        
+        T LoadResource(string key);
+        IEnumerable<Tuple<string, T>> LoadResources();
+        IEnumerable<T> LoadResources(IEnumerable<string> keys);
+
+        void Clear();
+    }
+}
---
+++ src/Pancakes.Engine.Core/Utilities/DotNetExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pancakes.Engine.Utilities
+{
+    /// <summary>
+    /// A collection of convenience methods for common .NET idioms.
+    /// </summary>
+    public static class DotNetExtensions
+    {
+        /// <summary>
+        /// A LINQ-like foreach loop.
+        /// </summary>
+        /// <typeparam name="T">Type of the enumeration.</typeparam>
+        /// <param name="list">Enumerable to iterate over.</param>
+        /// <param name="action">Action to perform each iteration.</param>
+        public static void ForEach<T>(this IEnumerable<T> list, Action<T> action)
+        {
+            foreach (var item in list)
+            {
+                action(item);
+            }
+        }
+
+        /// <summary>
+        /// Sets the given value for the given key.  If the key/value pair
+        /// doesn't exist, it will be added to the dictionary.
+        /// </summary>
+        /// <param name="dict"></param>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        public static void ReplaceOrAdd<T, V>(this Dictionary<T, V> dict, T key, V value)
+        {
+            V obj;
+            var has = dict.TryGetValue(key, out obj);
+
+            if (has)
+                dict[key] = value;
+            else
+                dict.Add(key, value);
+        }
+    }
+}
